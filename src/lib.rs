7726c3af72995e981a00552777a647aa45b89b67
DarccMan <darcy3057@gmail.com>
2022-09-23 21:50:23 +1000

    Test API

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use chrono::{Date, Datelike, NaiveDate, TimeZone, Utc};
+use rand::Rng;
+
+/// Date of first ever comic
+const FIRST_DATE: &str = "1978-06-19";
+/// Url of comic, without date
+const BASE_URL: &str = "https://cors.bridged.cc/https://www.gocomics.com/garfield/";
+
+/// Get random `Date<Utc>` between `FIRST_DATE` and current date (`Utc::now()`)
+pub fn random_date() -> Date<Utc> {
+  let num = rand::thread_rng().gen_range(
+    NaiveDate::parse_from_str(FIRST_DATE, "%Y-%m-%d")
+      .expect("First date not properly formatted")
+      .and_hms(0, 0, 0)
+      .timestamp()..=Utc::now().timestamp(),
+  );
+  Utc.timestamp(num, 0).date()
+}
+
+/// Fetch url of comic image file from `Date<Utc>`
+pub async fn get_comic_url(date: Date<Utc>) -> Result<String, String> {
+  let url = BASE_URL.to_string() + &date_to_string(date, "/");
+  let body = match fetch(&url).await {
+    Ok(url) => url,
+    Err(err) => return Err(err.to_string()),
+  };
+  let index = match body.find("https://assets.amuniversal.com") {
+    Some(i) => i,
+    None => return Err("Cannot find image in body".to_string()),
+  };
+  Ok(body[index..index + 63].to_string())
+}
+
+/// Convert `Date<Utc>` to string separated by given character
+pub fn date_to_string(date: Date<Utc>, sep: &str) -> String {
+  date.year().to_string() + sep + &date.month().to_string() + sep + &date.day().to_string()
+}
+
+/// Quick fetch function
+async fn fetch(url: &str) -> Result<String, reqwest::Error> {
+  Ok(
+    reqwest::Client::new()
+      .get(url)
+      .header(
+        "x-cors-grida-api-key",
+        "77a0175b-4435-49b0-ad18-52d2dea5a548",
+      )
+      .send()
+      .await?
+      .text()
+      .await?,
+  )
+  // Ok(reqwest::get(url).await?.text().await?)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,57 @@
 use yew::prelude::*;
+use yew_hooks::use_async;
 
 #[function_component(App)]
 fn app() -> Html {
+  let date = garf::random_date();
+
+  let state = use_async( garf::get_comic_url(date) );
+
+  let onclick = {
+    let state = state.clone();
+    Callback::from(move |_| {
+      state.run();
+    })
+  };
+
   html! {
-    <h1>{ "Hello World" }</h1>
+    <>
+      <h1>{ "Garf" }</h1>
+      <p>{ date }</p>
+      <button {onclick}>{ "Run" }</button>
+      <p>
+        {
+          if state.loading {
+            html! { "Loading..." }
+          } else {
+            html! { "Not loading." }
+          }
+        }
+      </p>
+      <p>
+        {
+          if let Some(error) = &state.error {
+            html! { error }
+          } else {
+            html! { "No error." }
+          }
+        }
+      </p>
+      <p>
+        {
+          if let Some(data) = &state.data {
+            html! { 
+              <>
+                { data }
+                <img src={ data.to_owned() } />
+              </>
+            }
+          } else {
+            html! { "No result." }
+          }
+        }
+      </p>
+    </>
   }
 }
 
